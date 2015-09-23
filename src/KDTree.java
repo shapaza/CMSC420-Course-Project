22a8531609c71fd52ce126e836338be8e25639cb
shapaza <wang_brent@yahoo.com>
2015-09-23 18:23:30 -0400

    Added comments and started writeToFile method

--- src/KDTree.java
+++ src/KDTree.java
@@ -1,33 +1,43 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * 
- * 
+ * This is the KDTree class, which will effectively function as a 3D tree for the sake of this project. 
+ * It is a binary tree in which every node is a 3-D node of the class recordNode, which itself contains an XLOC, YLOC, and time variable.
  * 
  * @author Brent Wang
  * @UID	   112860066
  */
 public class KDTree {
+	
 	private recordNode root;
 	
+	/**
+	 * Constructor
+	 *  
+	 */
 	public KDTree() {
 		root = null;
 	}
 	
 	/**
-	 * 
+	 * Calls printHelper, which then prints out the nodes of the tree in preorder traversal
+	 * Mainly used as a test method to see if the output is correct
 	 */
 	public void printTree() {
 		printHelper(root);
 	}
 	
 	/**
+	 * The printHelper method recursively prints out the nodes of the tree in preorder traversal
+	 * Printed in the format "[PARENT mobile ID] LCHILD/RCHILD [CHILD mobile ID]" for all children
+	 * If the current node does not have children, then it does not get printed.
 	 * 
-	 * 
-	 * @param curr
+	 * @param curr	the current recordNode we are at, for the sake of recursion
 	 */
 	private void printHelper(recordNode curr) {
 		if (curr == null)
@@ -47,10 +57,48 @@ public class KDTree {
 		printHelper(curr.getRight());
 	}
 	
+	public void writeToFile(String filePath) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+			writeToFileHelper(root, writer);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void writeToFileHelper(recordNode curr, BufferedWriter writer) {
+		if (curr == null)
+			return;
+		
+		if (curr.getLeft() != null) {
+			try {
+				writer.append(curr.getPhoneID() + " LCHILD " + curr.getLeft().getPhoneID());
+				writer.append("\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (curr.getRight() != null) {
+			try {
+				writer.append(curr.getPhoneID() + " RCHILD " + curr.getRight().getPhoneID());
+				writer.append("\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		writeToFileHelper(curr.getLeft(), writer);
+		writeToFileHelper(curr.getRight(), writer);
+	}
+	
 	/**
+	 * Insert function
+	 * If the tree is empty, make recordNode r the root node
+	 * Else, call insertHelper to insert r into the tree
 	 * 
-	 * 
-	 * @param r        the recordNode that we want to insert into the tree
+	 * @param r		the recordNode that we want to insert into the tree
 	 */
 	public void insert(recordNode r) {
 		if (root == null)
@@ -60,7 +108,8 @@ public class KDTree {
 	}
 	
 	/**
-	 * 
+	 * The insertHelper method recursively inserts recordNode r into the tree
+	 * It discriminates on the XLOC, YLOC, or time variable, depending on the level of the node we are currently at
 	 * 
 	 * @param curr     the current recordNode we are at, for the sake of recursion
 	 * @param r		   the recordNode that we are trying to insert into the tree
@@ -168,18 +217,23 @@ public class KDTree {
 	}
 	
 	/**
-	 * @param r
-	 * @return an integer representing the level of recordNode r in the current tree
+	 * The getLevel method
+	 * calls getLevelHelper to return the level of recordNode r
+	 * 
+	 * @param r	  the recordNode which we are trying to find the level of
+	 * @return an integer representing the level of recordNode r in the current tree or -1 if it's not in the tree
 	 */
 	public int getLevel(recordNode r) {
 		return getLevelHelper(root, r, 0);
 	}
 	
 	/**
-	 * @param curr
-	 * @param r
-	 * @param level
-	 * @return
+	 * 
+	 * 
+	 * @param curr   the current recordNode we are at, for the sake of recursion
+	 * @param r		 the recordNode which we are trying to find the level of
+	 * @param level  the int representing the current level we are at, which we will return if we find recordNode r
+	 * @return an integer representing the level of recordNode r in the current tree or -1 if it's not in the tree
 	 */
 	private int getLevelHelper(recordNode curr, recordNode r, int level) {
 		if (curr == null)
--- src/Runner.java
+++ src/Runner.java
@@ -3,11 +3,12 @@ public class Runner {
 
 	public static void main(String[] args) {
 		String workingDir = System.getProperty("user.dir") + "\\";
-		String fileName = "points";
-		String filePath = workingDir + fileName;
+		String inputFileName = "points";
+		String outputFileName = "output1";
+		String inputFilePath = workingDir + inputFileName;
 		
 		KDTree MyTree = new KDTree();
-		MyTree.insertFile(filePath);
+		MyTree.insertFile(inputFilePath);
 		
 		MyTree.printTree();
 	}
